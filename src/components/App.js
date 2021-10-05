f3d55c002f247b0d378c506bf4eee092c659246b
sunithapatel <s_shenoi@hotmail.com>
2021-10-05 16:18:31 -0400

    Create JokesGallery component and render jokes from a free API.

--- src/components/App.js
+++ src/components/App.js
@@ -2,36 +2,18 @@ import './App.css';
 import laughimage from '../assets/laugh.png';
 import laughinganimals from '../assets/laughing-animals.png';
 
+import JokesGallery from './JokesGallery';
+
 function App() {
     return (
         <main>
             <header>
                 <h1 className="question">Need Some Laughs?</h1>
                 <img src={laughimage} alt="Big laugh" />
-                <h2 className="message">Scroll through our random supply of jokes to get your fill today!</h2>
+                <h2 className="message">Scroll down for some jokes to get your fill today!</h2>
                 <img className="laughing-image" src={laughinganimals} alt="Cute laughing animals" />
             </header>
-            <section className="content">
-                <div className="jokes-gallery">
-                    <div className="joke-card">
-                        What do you call a fake noodle? An impasta.
-                    </div>
-                    <div className="joke-card">
-                        I decided to sell my Hoover… well it was just collecting dust.
-                    </div>
-                    <div className="joke-card">
-                        What did the hat say to the scarf? 
-                        <br /> 
-                        You can hang around. I'll just go on ahead.
-                    </div>
-                    <div className="joke-card">
-                        I don't trust stairs. They're always up to something.
-                    </div>
-                    <div className="joke-card">
-                        I've just written a song about a tortilla. Well, it is more of a rap really.
-                    </div>
-                </div>
-            </section>
+            <JokesGallery />
             <footer>
                 Created by Sunitha Patel 
                 <a href="https://github.com/sunithapatel/jokeshub">
---
+++ src/components/JokesGallery.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import './JokesGallery.css';
+
+class JokesGallery extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          jokes: [],
+          comic: null
+        };
+    }
+
+    componentDidMount() {
+        let headers = new Headers();
+        headers.append("Accept", "application/json");
+        headers.append("User-Agent", "My Jokes Hub (https://github.com/sunithapatel/jokeshub)");
+
+        fetch("https://icanhazdadjoke.com/search?limit=5", { method: 'Get', headers: headers })
+          .then(response => response.json())
+          .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        jokes: result.results
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            );
+    }
+
+    renderJokes() {
+        return (
+            this.state.jokes.map(joke => (
+                <div key={joke.id} className="joke-card">
+                    {joke.joke}
+                </div>
+            ))
+        );
+    }
+    
+    render() {
+        return (
+            <section className="jokes-gallery">
+                {this.renderJokes()}
+                <div className="jokes-source">
+                    <span>
+                        Source:
+                        <a href="https://icanhazdadjoke.com">
+                            <img className="logo" src="https://icanhazdadjoke.com/static/smile.svg" alt="icanhazdadjoke.com logo" height="24" width="24" />
+                            icanhazdadjoke.com
+                        </a>
+                    </span>
+                </div>
+            </section>
+        );
+    }
+
+
+}
+
+export default JokesGallery;
